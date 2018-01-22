863825cc3efc234e0bedbf1d8032fa8b02ab3054
P. Doyle <patrick.doyle126@gmail.com>
2018-01-22 16:44:37 -0600

    add

--- client/src/components/search/search.js
+++ client/src/components/search/search.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const Search = () =>
 
-	<div className="formGroup container-fluid">
+	<div className="formGroup container" style={{"border-style": "solid", "border-color":"Black", margin: "auto"}}>
+		<form>
 		<p style={{"text-align": "center"}}>Topic</p>
 		<input className="fromControl" style={{display:"block", margin: "auto"}}/><br/>
 		<p style={{"text-align": "center"}}>Start Year</p>
 		<input className="fromControl" style={{display:"block", margin: "auto"}}/><br/>
 		<p style={{"text-align": "center"}}>End Year</p>
 		<input className="fromControl" style={{display:"block", margin: "auto"}}/><br/>
+		<button style={{display:"block", margin: "auto"}}>Submit</button>
+
+		</form>
 	</div>
 
 export default Search;
